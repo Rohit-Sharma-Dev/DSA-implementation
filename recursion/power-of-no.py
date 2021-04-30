ae119be5805efa092f8bbdd69a321220f78b3082
rohit <61960550+rohitk7065@users.noreply.github.com>
2021-04-30 14:55:00 +0530

    Update power-of-no.py

--- recursion/power-of-no.py
+++ recursion/power-of-no.py
@@ -1,8 +1,10 @@
 def powerofno(num,power):
 
-    if power==0 or power==1:
+    if power==0:
+        return 1
+    elif power==1:
         return num
     else:
         return (num*powerofno(num,power-1))
 
-print(powerofno(5,2))
+print(powerofno(5,0))
